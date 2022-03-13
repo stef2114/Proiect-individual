f3f011101f0558c39a537a945ecaae8ff4ccb8bc
stef2114 <95346648+stef2114@users.noreply.github.com>
2022-03-13 08:27:46 +0200

    Add files via upload

---
+++ proiect3 - varianta3/src/main/java/ro/digitalnation/Proiect3Application.java
@@ -0,0 +1,13 @@
+package ro.digitalnation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proiect3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proiect3Application.class, args);
+	}
+
+}
